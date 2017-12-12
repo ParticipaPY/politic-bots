75f82a4ee6f354cfc0476659105069af4cf2aa3a
Jammily Ortigoza <jortigozaf@gmail.com>
2017-12-12 15:59:43 -0300

    Change to tweepy library Extract keywords and metadata from .csv file Maintain search context across API rate limit window

--- search_tweets.py
+++ search_tweets.py
@@ -1,9 +1,8 @@
 import json
-import threading
-
-from TwitterSearch import (
-    TwitterSearchOrder, TwitterSearchException, TwitterSearch
-    )
+import tweepy
+import csv
+import time
+from db_manager import add_tweet, get_db
 
 
 # Get configuration from file
@@ -13,39 +12,94 @@ def get_config(config_file):
     return config
 
 
-# Process tweets to show results in json format with one file per keyword
-def process_and_store(tweet, keyword):
-    keyword = keyword + ".txt"
-    with open(keyword, 'a') as f:
-        print(json.dumps(tweet), file=f)
+# Get keywords and metadata from csv file
+def parse_metadata():
+    keyword = []
+    k_metadata = []
+    with open('kwd_metadata.csv', 'r', encoding='utf-8') as f:
+        kfile = csv.DictReader(f)
+        for line in kfile:
+            keyword.append(line['keyword'])
+            k_metadata.append({
+                'partido_politico': line['partido_politico'],
+                'movimiento': line['movimiento'],
+                'lider_movimiento': line['lider_movimiento'],
+                'candidatura': line['candidatura']
+            })
+    return keyword, k_metadata
 
+# Add tweets to DB
+def process_and_store(tweet, keyword, ktype, k_metadata):
+    db = get_db()
+    date = time.strftime("%m/%d/%y")
+    add_tweet(db, json.dumps(tweet._json), ktype, keyword, date, k_metadata)
 
-def twitter_search(keyword, conf):
-    try:
-        tso = TwitterSearchOrder()
-        tso.set_keywords([keyword])
-        tso.set_language('es')  # Spanish tweets only
-        tso.set_include_entities(False)
-        # secret tokens
-        ts = TwitterSearch(
-            access_token=conf['twitter']['access_token'],
-            access_token_secret=conf['twitter']['access_token_secret'],
-            consumer_key=conf['twitter']['consumer_key'],
-            consumer_secret=conf['twitter']['consumer_secret']
-        )
-        # recollect tweets
-        for tweet in ts.search_tweets_iterable(tso):
-            process_and_store(tweet, keyword)
-
-    except TwitterSearchException as e:
-        print(e)
+# Search tweets
+def twitter_search(keyword, conf, ktype, k_metadata):
+    min_id = None
+    max_id = -1
+    count_tweets = 0
+    auth = tweepy.AppAuthHandler(
+        conf['twitter']['consumer_key'],
+        conf['twitter']['consumer_secret']
+    )
+    api = tweepy.API(
+        auth,
+        wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    while count_tweets < conf['max_tweets']:
+        try:
+            if max_id <= 0:
+                if not min_id:
+                    new_tweets = api.search(
+                        q=keyword,
+                        count=conf['tweets_qry'],
+                        locale='es'
+                    )
+                else:
+                    new_tweets = api.search(
+                        q=keyword,
+                        count=conf['tweets_qry'],
+                        since_id=min_id,
+                        locale='es'
+                    )
+            else:
+                if not min_id:
+                    new_tweets = api.search(
+                        q=keyword,
+                        count=conf['tweets_qry'],
+                        max_id=str(max_id - 1),
+                        locale='es'
+                    )
+                else:
+                    new_tweets = api.search(
+                        q=keyword,
+                        count=conf['tweets_qry'],
+                        max_id=str(max_id - 1),
+                        since_id=min_id,
+                        locale='es'
+                    )
+            if not new_tweets:
+                print("No more tweets found")
+                break
+            for tweet in new_tweets:
+                process_and_store(tweet, keyword, ktype, k_metadata)
+            count_tweets += len(new_tweets)
+            max_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+            # Exit if any error
+            print("Error: " + str(e))
+            break
+    print("Downloaded {0} tweets".format(count_tweets))
 
 
 if __name__ == "__main__":
-    threads = []
-    myconf = "config.json"
+    keyword, k_metadata = parse_metadata()
+    myconf = "test.json"
     configuration = get_config(myconf)
-    for i in configuration['keywords']:
-        t = threading.Thread(target=twitter_search, args=(i, configuration))
-        threads.append(t)
-        t.start()
+    for i, j in zip(keyword, k_metadata):
+        print("Searching tweets for %s" % i)
+        if '@' in i:
+            twitter_search(i, configuration, "user", j)
+        else:
+            twitter_search(i, configuration, "hashtag", j)
