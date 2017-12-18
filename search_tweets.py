07f2e7c2160c836f5749433466fd847b3464ca4a
Jammily Ortigoza <jortigozaf@gmail.com>
2017-12-18 16:04:22 -0300

    Change api.search for tweepy.Cursor

--- search_tweets.py
+++ search_tweets.py
@@ -13,10 +13,10 @@ def get_config(config_file):
 
 
 # Get keywords and metadata from csv file
-def parse_metadata():
+def parse_metadata(kfile):
     keyword = []
     k_metadata = []
-    with open('kwd_metadata.csv', 'r', encoding='utf-8') as f:
+    with open(kfile, 'r', encoding='utf-8') as f:
         kfile = csv.DictReader(f)
         for line in kfile:
             keyword.append(line['keyword'])
@@ -28,16 +28,16 @@ def parse_metadata():
             })
     return keyword, k_metadata
 
+
 # Add tweets to DB
 def process_and_store(tweet, keyword, ktype, k_metadata):
     db = get_db()
     date = time.strftime("%m/%d/%y")
     add_tweet(db, tweet._json, ktype, keyword, date, k_metadata)
 
+
 # Search tweets
 def twitter_search(keyword, conf, ktype, k_metadata):
-    min_id = None
-    max_id = -1
     count_tweets = 0
     auth = tweepy.AppAuthHandler(
         conf['twitter']['consumer_key'],
@@ -47,56 +47,30 @@ def twitter_search(keyword, conf, ktype, k_metadata):
         auth,
         wait_on_rate_limit=True,
         wait_on_rate_limit_notify=True)
-    while count_tweets < conf['max_tweets']:
-        try:
-            if max_id <= 0:
-                if not min_id:
-                    new_tweets = api.search(
-                        q=keyword,
-                        count=conf['tweets_qry'],
-                        locale='es'
-                    )
-                else:
-                    new_tweets = api.search(
-                        q=keyword,
-                        count=conf['tweets_qry'],
-                        since_id=min_id,
-                        locale='es'
-                    )
-            else:
-                if not min_id:
-                    new_tweets = api.search(
-                        q=keyword,
-                        count=conf['tweets_qry'],
-                        max_id=str(max_id - 1),
-                        locale='es'
-                    )
-                else:
-                    new_tweets = api.search(
-                        q=keyword,
-                        count=conf['tweets_qry'],
-                        max_id=str(max_id - 1),
-                        since_id=min_id,
-                        locale='es'
-                    )
-            if not new_tweets:
-                print("No more tweets found")
-                break
-            for tweet in new_tweets:
-                process_and_store(tweet, keyword, ktype, k_metadata)
-            count_tweets += len(new_tweets)
-            max_id = new_tweets[-1].id
-        except tweepy.TweepError as e:
-            # Exit if any error
-            print("Error: " + str(e))
-            break
+    i = 0
+    try:
+        for tweet in tweepy.Cursor(
+            api.search,
+            q=keyword,
+            count=conf['tweets_qry'],
+            locale='es',
+            tweet_mode='extended',
+            include_entities=True
+        ).items():
+            i += 1
+            process_and_store(tweet, keyword, ktype, k_metadata)
+        count_tweets += i
+        print(count_tweets)
+    except tweepy.TweepError as e:
+        # Exit if any error
+        print("Error: " + str(e))
     print("Downloaded {0} tweets".format(count_tweets))
 
 
 if __name__ == "__main__":
-    keyword, k_metadata = parse_metadata()
-    myconf = "test.json"
+    myconf = "config.json"
     configuration = get_config(myconf)
+    keyword, k_metadata = parse_metadata(configuration['metadata'])
     for i, j in zip(keyword, k_metadata):
         print("Searching tweets for %s" % i)
         if '@' in i:
