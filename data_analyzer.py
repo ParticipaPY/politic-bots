e2f5cbb17f8b7f5931f8d4cf176e262b24bcff14
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-12-19 17:44:35 -0300

    Improve the method to analyze the sentiment of tweets

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,9 +1,13 @@
+import indicoio
 import nltk
 import re
+from textblob import TextBlob
+
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-from textblob import TextBlob
+from utils import get_config
+
 
 
 def tokenize_and_remove_stop_words(text, specific_words_to_delete=[],
@@ -145,19 +149,87 @@ class SentimentAnalyzer():
         return self._tagged_docs
 
 
-def sentiment_analysis(db, query={}):
+def in_house_sentiment_analysis(text, language='spanish'):
+    sa = SentimentAnalyzer()
+    sa_result = sa.analyze_doc('text')
+    return {
+        'tono': sa_result[1],
+        'score': sa_result[2]
+    }
+
+
+def indicoio_sentiment_analysis(text, language='spanish', api_key=''):
+    indicoio.config.api_key = api_key
+    sa_result = indicoio.sentiment(text, language=language)
+    if sa_result <= 0.35:
+        tono = 'negative'
+    elif sa_result <= 0.65:
+        tono = 'neutral'
+    else:
+        tono = 'positive'
+    return {
+        'tono': tono,
+        'score': sa_result
+    }
+
+
+def get_analyzed_tweet(analyzed_tweets, id_tweet_to_search):
+    for analyzed_tweet in analyzed_tweets:
+        if id_tweet_to_search == analyzed_tweet['id']:
+            return analyzed_tweet
+    return None
+
+
+def update_sentimient_rts(db, pending_tweets):
+    for pending_tweet in pending_tweets:
+        rt = db.tweets.find({'tweet_obj.id_str': pending_tweet['id_rt']})
+        ot = db.tweets.find({'tweet_obj.id_str': pending_tweet['id_original']})
+        rt['sentimiento'] = ot['sentimiento']
+        db.tweets.save(rt)
+
+
+def sentiment_analysis(db, query={}, method='in_house', language='spanish'):
     """
     :param db: MongoDB database
     :param query: dictionary of <key, value> terms to be used in querying the db
     """
-    sa = SentimentAnalyzer()
-    tweet_regs = db.tweets.find(query)
 
+    tweet_regs = db.tweets.find(query)
+    analyzed_tweets = []
+    pending_tweets = []
+    config = get_config('config.json')
     for tweet_reg in tweet_regs:
         tweet = tweet_reg['tweet_obj']
-        sa_result = sa.analyze_doc(tweet['text'])
-        tweet_reg['sentiment'] = sa_result[1]
-        tweet_reg['score'] = sa_result[2]
+        if 'retweeted_status' in tweet.keys():
+            id_original_tweet = tweet['retweeted_status']['id_str']
+            analyzed_tweet = get_analyzed_tweet(analyzed_tweets, id_original_tweet)
+            if analyzed_tweet:
+                tweet_reg['sentimiento'] = analyzed_tweet['sentimiento']
+            else:
+                pending_tweets.append({
+                    'id_original': tweet['retweeted_status']['id_str'],
+                    'id_rt': tweet['id_str']
+                })
+            continue
+        if 'full_text' in tweet.keys():
+            tweet_text = tweet['full_text']
+        else:
+            tweet_text = tweet['text']
+        if method == 'in_house':
+            sentiment_result = in_house_sentiment_analysis(tweet_text)
+        elif method == 'indico':
+            sentiment_result = indicoio_sentiment_analysis(tweet_text,
+                                                           api_key=config['indicoio_key'],
+                                                           language=language)
+        else:
+            raise Exception('Sentiment analysis method unknown!')
+        tweet_reg['sentimiento'] = sentiment_result
+        db.tweets.save(tweet_reg)
+        analyzed_tweets.append({
+            'id': tweet['id_str'],
+            'texto': tweet_text,
+            'sentimiento': sentiment_result
+        })
+    update_sentimient_rts(db, pending_tweets)
 
-    return True
-
+    return analyzed_tweets
