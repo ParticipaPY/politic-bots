0f4a9d9dccbc11cc35df4227d2570f4409e25b85
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-12-19 17:44:08 -0300

    Move utilitarian methods to utils module

--- search_tweets.py
+++ search_tweets.py
@@ -1,32 +1,7 @@
-import json
 import tweepy
-import csv
 import time
 from db_manager import add_tweet, get_db
-
-
-# Get configuration from file
-def get_config(config_file):
-    with open(config_file) as f:
-        config = json.loads(f.read())
-    return config
-
-
-# Get keywords and metadata from csv file
-def parse_metadata(kfile):
-    keyword = []
-    k_metadata = []
-    with open(kfile, 'r', encoding='utf-8') as f:
-        kfile = csv.DictReader(f)
-        for line in kfile:
-            keyword.append(line['keyword'])
-            k_metadata.append({
-                'partido_politico': line['partido_politico'],
-                'movimiento': line['movimiento'],
-                'lider_movimiento': line['lider_movimiento'],
-                'candidatura': line['candidatura']
-            })
-    return keyword, k_metadata
+from utils import get_config, parse_metadata
 
 
 # Add tweets to DB
