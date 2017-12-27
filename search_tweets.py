fc8c92d721bafbb5b6af42dcd6b8bfda56f07606
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-12-27 12:04:50 -0300

    Add a call to the function that evaluates the relevance of tweets

--- search_tweets.py
+++ search_tweets.py
@@ -1,18 +1,21 @@
 import tweepy
 import time
+import logging
 from db_manager import add_tweet, get_db
+from data_wrangler import TweetEvaluator
 from utils import get_config, parse_metadata
+
+logging.basicConfig(filename='politic_bots.log', level=logging.DEBUG)
 
 
 # Add tweets to DB
-def process_and_store(tweet, keyword, ktype, k_metadata):
-    db = get_db()
+def process_and_store(db, tweet, keyword, ktype, k_metadata):
     date = time.strftime("%m/%d/%y")
     add_tweet(db, tweet._json, ktype, keyword, date, k_metadata)
 
 
 # Search tweets
-def twitter_search(keyword, conf, ktype, k_metadata):
+def twitter_search(db, keyword, conf, ktype, k_metadata):
     count_tweets = 0
     auth = tweepy.AppAuthHandler(
         conf['twitter']['consumer_key'],
@@ -33,22 +36,26 @@ def twitter_search(keyword, conf, ktype, k_metadata):
             include_entities=True
         ).items():
             i += 1
-            process_and_store(tweet, keyword, ktype, k_metadata)
+            process_and_store(db, tweet, keyword, ktype, k_metadata)
         count_tweets += i
         print(count_tweets)
     except tweepy.TweepError as e:
         # Exit if any error
-        print("Error: " + str(e))
-    print("Downloaded {0} tweets".format(count_tweets))
+        logging.error('Error: ' + str(e))
+    logging.info('Downloaded {0} tweets'.format(count_tweets))
 
 
 if __name__ == "__main__":
-    myconf = "config.json"
+    myconf = 'config.json'
     configuration = get_config(myconf)
     keyword, k_metadata = parse_metadata(configuration['metadata'])
+    db = get_db()
     for i, j in zip(keyword, k_metadata):
-        print("Searching tweets for %s" % i)
+        logging.info('Searching tweets for %s' % i)
         if '@' in i:
-            twitter_search(i, configuration, "user", j)
+            twitter_search(db, i, configuration, 'user', j)
         else:
-            twitter_search(i, configuration, "hashtag", j)
+            twitter_search(db, i, configuration, 'hashtag', j)
+    logging.info('Evaluating the relevance of the new tweets...')
+    te = TweetEvaluator()
+    te.identify_relevant_tweets(db)
