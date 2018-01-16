1966021cc838d27f1d765fea25671dd56c3ddfcc
Jorge Saldivar <jorgesaldivar@gmail.com>
2018-01-16 10:26:55 -0300

    Add a function and a class to compute the local date of tweets

--- data_wrangler.py
+++ data_wrangler.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import datetime, timedelta, tzinfo
 from db_manager import do_search
 from utils import get_user_handlers_and_hashtags, parse_metadata, get_config
 import re
@@ -303,3 +304,40 @@ class HashtagDiscoverer:
                     for k in sorted(coccurence_hashtags_dict, key=coccurence_hashtags_dict.get, reverse=True)]
         else:
             return coccurence_hashtags_dict
+
+# Paraguayan Timezone
+class UTC4(tzinfo):
+    def utcoffset(self, dt):
+        return timedelta(hours=-4) + self.dst(dt)
+
+    def dst(self, dt):
+        # DST starts last Sunday in October
+        d = datetime(dt.year, 11, 1)
+        self.dston = d - timedelta(days=d.weekday() + 1)
+        # ends last Sunday in March
+        d = datetime(dt.year+1, 4, 1)
+        self.dstoff = d - timedelta(days=d.weekday() + 1)
+        if self.dston <= dt.replace(tzinfo=None) < self.dstoff:
+            return timedelta(hours=1)
+        else:
+            return timedelta(0)
+
+    def tzname(self,dt):
+        return "GMT -4"
+
+
+def extract_date_from_tweets(db):
+    PYT = UTC4()
+    query = {
+        'tweet_py_date': {'$exists': 0}
+    }
+    s_objs = do_search(db, query, only_relevant_tws=False)
+    for s_obj in s_objs:
+        tweet = s_obj['tweet_obj']
+        str_pub_dt = tweet['created_at']
+        pub_dt = datetime.strptime(str_pub_dt, '%a %b %d %H:%M:%S %z %Y')
+        # convert to paraguayan timezone
+        py_pub_dt = pub_dt.astimezone(PYT)
+        s_obj['tweet_py_date'] = datetime.strftime(py_pub_dt, '%m/%d/%y')
+        db.tweets.save(s_obj)
+    return
