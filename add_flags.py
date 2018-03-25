3ad43654e2f8c7e5ee922462459086743d1a4d91
Jorge Saldivar <jorgesaldivar@gmail.com>
2018-03-25 17:20:19 -0400

    Add a new method to get entities of RTs
    Minor changes:
    1. Undo changes in the method create_flags and fix parameters
    2. Rename variables in the method add_values_to_flags to facilitate the review

--- add_flags.py
+++ add_flags.py
@@ -6,41 +6,54 @@ def create_flag(metadata, val):
     headers = []
     saved = 0
     columns = defaultdict(list)  # each value in each column is appended to a list
-    for key, value in metadata.items():
-        columns[key].append(value)
-        if not saved:
-            flags[key]= {}
-            headers.append(key)
-    for i in headers:
-        for j in set(columns[i]):
-            if i == val:
-                flags[i] = []
-            elif j != "":
-                flags[i][j] = 0
+    for row in metadata:
+        for key, value in row.items():
+            if value:
+                columns[key].append(value.strip())
+            else:
+                columns[key].append(value)
+            if not saved:
+                flags[key] = {}
+                headers.append(key.strip())
+        saved = 1
+    for header in headers:
+        for column in set(columns[header]):
+            if header == val:
+                flags[header] = []
+            elif column != "":
+                flags[header][column] = 0
             else:
                 continue
     return flags, headers
 
 
-def get_entities_data(tweet):
-    hashtags = []
-    user_mentions = []
-    e_hashtags = tweet['entities']['hashtags']
-    e_user_mentions = tweet['entities']['user_mentions']
-    for l in e_hashtags:
-        hashtags.append(l['text'])
-    for m in e_user_mentions:
-        user_mentions.append(m['screen_name']) 
-    return set(hashtags+user_mentions)
+def do_get_entities_tweet(tweet):
+    entities = set()
+    hashtags = tweet['entities']['hashtags']
+    user_mentions = tweet['entities']['user_mentions']
+    for hashtag in hashtags:
+        entities.add(hashtag['text'])
+    for user_mention in user_mentions:
+        entities.add(user_mention['screen_name'])
+    return entities
 
 
-def add_values_to_flags(flags, key, entities, metadata, val):
-    for word in entities:
+def get_entities_tweet(tweet):
+    entities = do_get_entities_tweet(tweet)
+    # if the tweet is a RT, add also the entities
+    # of the original tweet
+    if tweet['retweeted_status']:
+        entities.update(do_get_entities_tweet(tweet['retweeted_status']))
+    return entities
+
+
+def add_values_to_flags(flags, entities, metadata, val):
+    for entity in entities:
         for row in metadata:
-            if row[val].lower() == word.lower() or row[val] == '@'+word:
+            if row[val].lower() == entity.lower() or row[val] == '@'+entity:
                 for k, v in row.items():
                     if k == val:
-                        flags[k].append(word)
+                        flags[k].append(entity)
                     elif v != "":
                         flags[k][v] += 1
-    return {'flag':flags}
+    return {'flag': flags}
