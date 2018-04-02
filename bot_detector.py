12ab7950bda325def8571122821a4002b4cd4581
Jorge Saldivar <jorgesaldivar@gmail.com>
2018-04-02 18:55:15 -0400

    Big code refactor
    Create a class and add methods to it

--- bot_detector.py
+++ bot_detector.py
@@ -1,117 +1,127 @@
 import json
 import tweepy
-import datetime
-from pymongo import MongoClient
 from db_manager import DBManager
-from network_analysis import NetworkAnalyzer
 
-def get_config(config_file):
-    with open(config_file) as f:
-        config = json.loads(f.read())
-    return config
 
-def get_db():
-    config = get_config('config.json')
-    host = config['mongo']['host']
-    port = config['mongo']['port']
-    client = MongoClient(host + ':' + port)
-    db = client[config['mongo']['db_name']]
+class BotDetector:
+    __dbm_tweets = DBManager('tweets')
+    __dbm_users = DBManager('users')
+    __api = None
+    __conf = None
+    __implemented_heuristics = 7
 
-    return db
+    def __init__(self, name_config_file='config.json'):
+        self.__conf = self.__get_config(name_config_file)
+        auth = tweepy.AppAuthHandler(
+            self.__conf['twitter']['consumer_key'],
+            self.__conf['twitter']['consumer_secret']
+        )
+        self.__api = tweepy.API(
+            auth,
+            wait_on_rate_limit=True,
+            wait_on_rate_limit_notify=True)
 
-def parse_date(date):
-    sd = date.split(" ")
-    date = {"date": ' '.join(sd[0:3]), "time": sd[3], "year": sd[5]}
-    return date
+    def __get_config(self, config_file):
+        with open(config_file) as f:
+            config = json.loads(f.read())
+        return config
 
-def get_user(screen_name):
-    db = get_db()
-    user = db.tweets.find({"tweet_obj.user.screen_name": screen_name})[0]
-    return user['tweet_obj']['user']
+    def __parse_date(self, date):
+        split_date = date.split(' ')
+        date = {'date': ' '.join(split_date[0:3]), 'time': split_date[3],
+                'year': split_date[5]}
+        return date
 
-def get_timeline(api, user):
-    tl = []
-    for status in tweepy.Cursor(api.user_timeline, screen_name=user).items():
-        timeline_data = {"tweet_creation": status._json['created_at'], "text": status._json['text']}
-        tl.append(timeline_data)
-    return tl
+    def __get_user(self, screen_name):
+        user = self.__dbm_tweets.search({'tweet_obj.user.screen_name': screen_name})[0]
+        return user['tweet_obj']['user']
 
-def creation_date(creation, current_year): # Account creation date
-    if int(creation['year']) < current_year or int(creation['year']) < current_year -1:
-        return 0
-    else:
-        print("creation", creation['year'])
-        return 1
+    # Get tweets in the timeline of a given user
+    def __get_timeline(self, user):
+        timeline = []
+        for status in tweepy.Cursor(self.__api.user_timeline, screen_name=user).items():
+            timeline_data = {'tweet_creation': status._json['created_at'],
+                             'text': status._json['text']}
+            timeline.append(timeline_data)
+        return timeline
 
-def is_retweet_bot(tl): # Retweet bot
-    i = j = 0
-    for line in tl:
-        i += 1
-        if "RT" in line['text']:
-            j += 1
-    return 1 if 90 >= (100*j)/i else 0
-    
+    # Check when the account was created
+    def __creation_date(self, creation, current_year):
+        if int(creation['year']) < current_year or int(creation['year']) < current_year -1:
+            return 0
+        else:
+            return 1
 
-def default_twitter_account(user):
-    count = 0
-    if user['default_profile'] is True: #Default twitter profile
-        count += 1
-    if user['default_profile_image'] is True: #Default profile image
-        count += 1
-    if user['profile_use_background_image'] is True: # Background image
-        count += 1
-    if user['description'] == "": # None description
-        count += 1
-    return count
-    
-def location(user): # Absence of geographical metadata
-    if user['location'] == "":
-        return 1
-    else:
-        return 0
+    # Check the number of retweets in a given timeline
+    def __is_retweet_bot(self, timeline):
+        i = j = 0
+        for line in timeline:
+            i += 1
+            if 'RT' in line['text']:
+                j += 1
+        return 1 if 90 >= (100*j)/i else 0
 
-def followers_ratio(user): #Ratio between followers/friends
-    ratio = int(user['followers_count'])/int(user['friends_count'])
-    if ratio < 0.4:
-        return 1
-    else: 
-        return 0
+    # Check the presence/absent of default elements in the profile of a given user
+    def __default_twitter_account(self, user):
+        count = 0
+        # Default twitter profile
+        if user['default_profile'] is True:
+            count += 1
+        # Default profile image
+        if user['default_profile_image'] is True:
+            count += 1
+        # Background image
+        if user['profile_use_background_image'] is True:
+            count += 1
+        # None description
+        if user['description'] == '':
+            count += 1
+        return count
 
-def bot_detector(conf, users):
-    auth = tweepy.AppAuthHandler(
-        conf['twitter']['consumer_key'],
-        conf['twitter']['consumer_secret']
-    )
-    api = tweepy.API(
-        auth,
-        wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
-    characterisctics = 7 # number of analyzed characteristics
-    bot = 0
-    for user in users:
-        print(user)
-        data = get_user(user)
-        tl = get_timeline(api, user)
-        bot =+ is_retweet_bot(tl)
-        bot = bot + creation_date(parse_date(data['created_at']), conf['current_year'])
-        bot = bot + default_twitter_account(data)
-        bot = bot + location(data)
-        bot = bot + followers_ratio(data)
-        print("Existe un %s%% de probabilidad que el usuario %s sea un Bot!" % (round((bot/characterisctics)*100, 2), user))
+    # Check the absence of geographical metadata in the profile of a given user
+    def __location(self, user):
+        if user['location'] == '':
+            return 1
+        else:
+            return 0
+
+    # Compute the ratio between followers/friends of a given user
+    def __followers_ratio(self, user):
+        ratio = int(user['followers_count'])/int(user['friends_count'])
+        if ratio < 0.4:
+            return 1
+        else:
+            return 0
+
+    def compute_bot_probability(self, users):
+        bot_score = 0
+        for user in users:
+            print('Computing the probability of the user {0}'.format(user))
+            # Get information about the user, check
+            # https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/user-object
+            # to understand the data of users available in the tweet
+            # objects
+            data = self.__get_user(user)
+            # Using the Twitter API get tweets of the user's timeline
+            timeline = self.__get_timeline(user)
+            # Check heuristics
+            bot_score += self.__is_retweet_bot(timeline)
+            bot_score = bot_score + self.__creation_date(self.__parse_date(data['created_at']),
+                                                         self.__conf['current_year'])
+            bot_score = bot_score + self.__default_twitter_account(data)
+            bot_score = bot_score + self.__location(data)
+            bot_score = bot_score + self.__followers_ratio(data)
+            print('There are a {0}% of probability that the user {1} would be bot'.format(
+                  round((bot_score/self.__implemented_heuristics)*100, 2), user))
+
 
 if __name__ == "__main__":
     myconf = 'config.json'
-    l_usr = []
-    configuration = get_config(myconf)
-    dbm = DBManager('tweets') 
     # To extract and analyzed all users from DB
     # users = dbm.get_unique_users() #get users from DB
     # for u in users:
     #    l_usr.append(u['screen_name'])
-
-    # list of samples
-    l_usr = ['CESARSANCHEZ553', 'Paraguaynosune', 'Solmelga', 'SemideiOmar']
-    bot_detector(configuration, l_usr)
-    
-
-    
+    # sample of users
+    users = ['CESARSANCHEZ553', 'Paraguaynosune', 'Solmelga', 'SemideiOmar']
+    bot_detector = BotDetector(myconf)
+    bot_detector.compute_bot_probability(users)
