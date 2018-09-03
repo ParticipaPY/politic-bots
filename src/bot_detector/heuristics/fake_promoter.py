1868bb8ab0cf0fdc691315049678707459366a1b
Jorge Saldivar <jorgesaldivar@gmail.com>
2018-09-03 17:09:27 -0400

    Improve code primary by removing unneccesary new lines

--- src/bot_detector/heuristics/fake_promoter.py
+++ src/bot_detector/heuristics/fake_promoter.py
@@ -61,23 +61,17 @@ def append_bot_detector_pbbs(bot_detector, dbm_users, dbm_tweets, users, NUM_USE
         user_interactions = user['interactions']
         user_screen_name = user['screen_name']
         if user_screen_name in ignore_list:
-            print('User: {} is in the ignore_list. Ignoring him...\n.'
-                .format(user))
+            print('User: {} is in the ignore_list'.format(user))
             continue
 
         user_record = dbm_users.find_record({'screen_name': user_screen_name})
         if 'bot_detector_pbb' in user_record.keys():
-            print('Pbb already calculated for user {} = {}.\n'
-                .format(user_screen_name, user_record['bot_detector_pbb']))
+            print('Pbb already calculated for user {} = {}.\n'.format(user_screen_name, user_record['bot_detector_pbb']))
         else:
             # Compute the bot_detector_pbb
             # for the current user and store it into the db
-            user_pbb = (
-                bot_detector.compute_bot_probability(
-                        [user_screen_name], False))
-            dbm_users.update_record(
-                {'screen_name': user_screen_name}
-                , {'bot_detector_pbb': user_pbb[user_screen_name]})
+            user_pbb = bot_detector.compute_bot_probability([user_screen_name], False)
+            dbm_users.update_record({'screen_name': user_screen_name}, {'bot_detector_pbb': user_pbb[user_screen_name]})
 
         # Create a list with the screen_name of each interacted user.
         # Temporarily, the list only will have users
@@ -86,42 +80,28 @@ def append_bot_detector_pbbs(bot_detector, dbm_users, dbm_tweets, users, NUM_USE
         interacted_users_count = 0
         # Iterate over the user_interactions
         # in interaction-total-descending order 
-        for interacted_user in sorted(
-                user_interactions
-                , key=lambda interactions: \
-                    user_interactions[interactions]['total']
-                , reverse=True):
+        for interacted_user in sorted(user_interactions, key=lambda interactions:
+                                      user_interactions[interactions]['total'], reverse=True):
             if interacted_users_count >= NUM_INTERACTED_USERS_UPDATE:
                 break
             interactions = user_interactions[interacted_user]
-            print('No. interactions with {}: {}.\n'
-                .format(interacted_user, interactions['total']))
+            print('No. interactions with {}: {}.\n'.format(interacted_user, interactions['total']))
             if interacted_user in ignore_list:
-                print('User: {} is in the ignore_list. Ignoring him...\n.'
-                    .format(interacted_user))
+                print('User: {} is in the ignore_list'.format(interacted_user))
                 continue
-            # If pbb already computed
-            # for the interacted user in question,
-            # no need to re-compute it
-            interacted_user_record = (
-                dbm_users.find_record({'screen_name': interacted_user}))
+            # If pbb already computed for the interacted user,no need to re-compute it
+            interacted_user_record = dbm_users.find_record({'screen_name': interacted_user})
             if 'bot_detector_pbb' in interacted_user_record.keys(): 
                 print('Pbb already calculated for interacted_user {} = {}.\n'
-                    .format(interacted_user
-                        , interacted_user_record['bot_detector_pbb'])) 
-                new_values = {'interactions.{0}.bot_detector_pbb'
-                  .format(interacted_user)
-                  : interacted_user_record['bot_detector_pbb']} 
-                dbm_users.update_record(
-                    {'screen_name': user_screen_name}, new_values)
-                continue 
+                      .format(interacted_user, interacted_user_record['bot_detector_pbb']))
+                new_values = {'interactions.{0}.bot_detector_pbb'.format(interacted_user):
+                              interacted_user_record['bot_detector_pbb']}
+                dbm_users.update_record({'screen_name': user_screen_name}, new_values)
+                continue
             # If pbb hasn't been computed yet 
-            interacted_user_db_tweets_count = (
-                dbm_tweets.search(
-                    {'tweet_obj.user.screen_name': interacted_user}).count())
+            interacted_user_db_tweets_count = dbm_tweets.search({'tweet_obj.user.screen_name': interacted_user}).count()
             print('User: {2}. Interacted_user: {0}. DB_Count: {1}.\n'\
-                .format(interacted_user, interacted_user_db_tweets_count
-                    , user_screen_name))
+                .format(interacted_user, interacted_user_db_tweets_count, user_screen_name))
             if interacted_user_db_tweets_count > 0:
                 interacted_users_count += 1
                 print('Pbb will be calculated for her/him.\n')
@@ -129,23 +109,15 @@ def append_bot_detector_pbbs(bot_detector, dbm_users, dbm_tweets, users, NUM_USE
     
         # Then a dictionary that maps
         # each screen_name with its bot_detector_pbb
-        interacted_users_pbbs = (
-            bot_detector.compute_bot_probability(interacted_users, False))
+        interacted_users_pbbs = bot_detector.compute_bot_probability(interacted_users, False)
         # And then we store the pbbs into the db
-        for interacted_user, bot_detector_pbb \
-                in interacted_users_pbbs.items():
-            new_values = {
-                'interactions.{0}.bot_detector_pbb'
-                .format(interacted_user): bot_detector_pbb}
-            print(
-                "Updating {}.{}\n".format(user_screen_name, repr(new_values)))
-            dbm_users.update_record(
-                {'screen_name': user_screen_name}, new_values)
+        for interacted_user, bot_detector_pbb in interacted_users_pbbs.items():
+            new_values = {'interactions.{0}.bot_detector_pbb'.format(interacted_user): bot_detector_pbb}
+            print("Updating {}.{}\n".format(user_screen_name, repr(new_values)))
+            dbm_users.update_record({'screen_name': user_screen_name}, new_values)
             # Since we've already computed the pbbs for this user
             # and they lack that attribute 
-            dbm_users.update_record(
-                {'screen_name': interacted_user}
-                , {'bot_detector_pbb': bot_detector_pbb})
+            dbm_users.update_record({'screen_name': interacted_user}, {'bot_detector_pbb': bot_detector_pbb})
             print("Sleeping for 5 seconds...\n")
             time.sleep(5)
 
