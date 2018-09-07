72567b55faa24dc2482fa366f319ab91ef8002cd
Jorge Saldivar <jorgesaldivar@gmail.com>
2018-09-07 18:36:27 -0400

    Remove temporal conditionals used to collect tweets for the presidential elections

--- src/tweet_collector/run.py
+++ src/tweet_collector/run.py
@@ -7,24 +7,20 @@ from src.utils.data_wrangler import TweetEvaluator
 
 logging.basicConfig(filename='politic_bots.log', level=logging.DEBUG)
 
-if __name__ == "__main__":
-    myconf = '../config.json'
-    configuration = get_config(myconf)
+if __name__ == '__main__':
+    conf_file = 'src/config.json'
+    configuration = get_config(conf_file)
     credentials = {'key': configuration['twitter']['consumer_key'],
                    'secret': configuration['twitter']['consumer_secret']}
     keyword, k_metadata = parse_metadata(configuration['metadata'])
     dbm = DBManager('tweets')
     tm = TwitterAPIManager(credentials, dbm)
     for current_keyword, keyword_row in zip(keyword, k_metadata):
-        if keyword_row['tipo_keyword'] == "org" or keyword_row['tipo_keyword'] == "general" or \
-           keyword_row['tipo_keyword'] == "personal":
-            logging.info('Searching tweets for %s' % current_keyword)
-            if '@' in current_keyword:
-                tm.search_tweets(configuration['tweets_qry'], current_keyword, 'user', k_metadata)
-            else:
-                tm.search_tweets(configuration['tweets_qry'], current_keyword, 'hashtag', k_metadata)
+        logging.info('Searching tweets for %s' % current_keyword)
+        if '@' in current_keyword:
+            tm.search_tweets(configuration['tweets_qry'], current_keyword, 'user', k_metadata)
         else:
-            continue
+            tm.search_tweets(configuration['tweets_qry'], current_keyword, 'hashtag', k_metadata)
     logging.info('Evaluating the relevance of the new tweets...')
     te = TweetEvaluator()
     te.identify_relevant_tweets()
