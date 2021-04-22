5141a83fd0e131f36b5c03d5b59f1fd47c8cace3
Jorge Saldivar <jorgesaldivar@gmail.com>
2021-04-22 14:53:42 +0200

    Add call to commands that add the fields type and complete_text to the tweet object

--- src/run.py
+++ src/run.py
@@ -12,7 +12,7 @@ from src.analyzer.network_analysis import NetworkAnalyzer
 from src.analyzer.data_analyzer import SentimentAnalysis
 from src.tweet_collector.twitter_api_manager import TwitterAPIManager
 from src.utils.db_manager import DBManager
-from src.utils.data_wrangler import TweetEvaluator
+from src.utils.data_wrangler import TweetEvaluator, add_complete_text_attr, add_tweet_type_attr
 from src.utils.utils import get_config, parse_metadata
 
 logging.basicConfig(filename=str(pathlib.Path(__file__).parents[0].joinpath('politic_bots.log')), level=logging.DEBUG)
@@ -69,7 +69,9 @@ def create_db_users():
 @click.option('--interaction_net', help='Generate the interaction network', default=False, is_flag=True)
 @click.option('--flag_tweets', help='Identify and flag relevant tweets', default=False, is_flag=True)
 @click.option('--db_users', help='Create a database of users', default=False, is_flag=True)
-def run_task(collect_tweets, sentiment_analysis, interaction_net, flag_tweets, db_users):
+@click.option('--add_complete_text', help='Add attribute complete text', default=False, is_flag=True)
+@click.option('--add_type', help='Add attribute complete text', default=False, is_flag=True)
+def run_task(collect_tweets, sentiment_analysis, interaction_net, flag_tweets, db_users, add_complete_text, add_type):
     if collect_tweets:
         do_tweet_collection()
     elif sentiment_analysis:
@@ -80,6 +82,10 @@ def run_task(collect_tweets, sentiment_analysis, interaction_net, flag_tweets, d
         build_interaction_net()
     elif db_users:
         create_db_users()
+    elif add_complete_text:
+        add_complete_text_attr()
+    elif add_type:
+        add_tweet_type_attr()
     else:
         click.UsageError('Illegal user: Please indicate a running option. Type --help for more information of '
                          'the available options')
